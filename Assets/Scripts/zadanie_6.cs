d2351ae331399120dc07ccb657f0a64fc737138e
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2025-10-24 21:12:47 +0200

    dodane lerp w zad 6

--- Assets/Scripts/zadanie_6.cs
+++ Assets/Scripts/zadanie_6.cs
@@ -1,7 +1,14 @@
+using System;
 using UnityEngine;
 
 public class zadanie_6 : MonoBehaviour
 {
+    public Transform target;
+    float smoothTime = 0.3f;
+    float yVelocity = 0.5f;
+    private float wobbleMin = -3.0f;
+    private float wobbleMax = 3.0f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,13 +16,10 @@ public class zadanie_6 : MonoBehaviour
     }
 
     // Update is called once per frame
-    public Transform target;
-    float smoothTime = 0.3f;
-    float yVelocity = 0.0f;
-
     void Update()
     {
         float newPosition = Mathf.SmoothDamp(transform.position.y, target.position.y, ref yVelocity, smoothTime);
         transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+        transform.position = new Vector3(Mathf.Lerp(wobbleMin, wobbleMax, yVelocity), transform.position.y, transform.position.z);
     }
 }
