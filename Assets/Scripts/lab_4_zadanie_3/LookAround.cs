593e0ae411a43c410cb1f0621d64be982759ce92
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2025-10-26 19:58:11 +0100

    kolejne zadanie

---
+++ Assets/Scripts/lab_4_zadanie_3/LookAround.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    // ruch wokół osi Y będzie wykonywany na obiekcie gracza, więc
+    // potrzebna nam referencja do niego (konkretnie jego komponentu Transform)
+    public Transform player;
+
+    public float sensitivity = 200f;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        // zablokowanie kursora na środku ekranu, oraz ukrycie kursora
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // pobieramy wartości dla obu osi ruchu myszy
+        float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        // wykonujemy rotację wokół osi Y
+        player.Rotate(Vector3.up * mouseXMove);
+
+        // ograniczenie widoku do podanego zakresu
+        xRotation -= mouseYMove;
+        xRotation = Mathf.Clamp(xRotation, -80f, 30f);
+        // a dla osi X obracamy kamerę dla lokalnych koordynatów
+        transform.localEulerAngles = new Vector3(xRotation, 0f, 0f);
+
+    }
+}
